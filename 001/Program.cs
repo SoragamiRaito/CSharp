fd147576ec8b7a3a2376e0517d0ca34cc54be414
SoragamiRaito <reiokikousen@gmail.com>
2023-05-26 16:52:56 +0900

    add 001

---
+++ 001/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("あなたの名前は？: ");
+var name = Console.ReadLine();
+
+Console.Write("あなたの年齢は？: ");
+var age = int.Parse(Console.ReadLine());
+
+Console.WriteLine("{0}({1}歳)さん、ようこそお越しくださいました。", name, age);
